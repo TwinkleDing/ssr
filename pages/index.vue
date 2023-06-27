acc42b289850cae75b590816316693e461b70102
dingyuliang <dingyuliang>
2023-06-27 14:22:57 +0800

    自定义layout

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,20 @@
   <div>
     <h1>Welcome to the homepage</h1>
     <nuxt-link to="/about">about</nuxt-link>
-    <AppAlert>
-      This is an auto-imported component
-    </AppAlert>
+    <AppAlert> This is an auto-imported component </AppAlert>
+    Page visits: {{ count }}
   </div>
 </template>
 <script lang="ts" setup>
 const appConfig = useAppConfig();
 console.log(appConfig.title);
+const { data: count } = await useFetch("http://localhost:6677/api/menuRouter");
+console.log(count.value);
+
+await useAsyncData(() => $fetch("http://localhost:6677/api/menuRouter"), {
+  transform: (ss) => console.log(ss),
+});
+definePageMeta({
+  layout: "custom",
+});
 </script>
